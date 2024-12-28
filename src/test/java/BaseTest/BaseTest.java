31efd2ca4a2d818ca1dbdfb1a61656c6122891ef
Vamsheedhar Reddy <geethanjali.c2811@gmail.com>
2024-12-28 13:01:53 -0600

    Made some minor changes

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -9,16 +9,22 @@ import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import org.testng.annotations.AfterClass;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 
-import java.time.Duration;
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 
 public class BaseTest {
 
     private static ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
-
+    ExtentSparkReporter extentSparkReporter;
+    protected ExtentReports extentReports;
+    public static ExtentTest extentTest;
+    
     public void setUp(String browser) {
         if (driverThreadLocal.get() == null) {
             WebDriver driver;
@@ -40,6 +46,41 @@ public class BaseTest {
         }
     }
 
+    @BeforeTest
+    public void startReporter()
+    {
+        extentSparkReporter  = new ExtentSparkReporter(System.getProperty("user.dir") + "/test-output/extentReport.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(extentSparkReporter);
+
+
+        //configuration items to change the look and feel
+        //add content, manage tests etc
+        extentSparkReporter.config().setDocumentTitle("Simple Automation Report");
+        extentSparkReporter.config().setReportName("Test Report");
+        extentSparkReporter.config().setTheme(Theme.STANDARD);
+        extentSparkReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+    }
+    
+    @AfterMethod
+    public void getResult(ITestResult result) {
+        if(result.getStatus() == ITestResult.FAILURE) {
+            extentTest.log(Status.FAIL,result.getThrowable());
+        }
+        else if(result.getStatus() == ITestResult.SUCCESS) {
+            extentTest.log(Status.PASS, result.getTestName());
+        }
+        else {
+            extentTest.log(Status.SKIP, result.getTestName());
+        }
+    }
+    
+    @AfterTest
+    public void compleExtentReports() {
+        //to write or update test information to the reporter
+        extentReports.flush();
+    }
+    
     public WebDriver getDriver() {
         return driverThreadLocal.get();
     }
